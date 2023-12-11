84c761d90a9b8b88d095326ac8293a8987e57cff
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-10 17:32:55 -0800

    strctured my directory

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+function writeFile (fileName, answers) {
+    let svgString = ""; 
+    svgString = 
+}
