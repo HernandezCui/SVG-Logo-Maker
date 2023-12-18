dfe731eb2671a61fa324a83e5a8cd4dae4ea69f0
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-17 17:25:48 -0800

    included class shape and functions

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const { Triangle, Circle, Square } = require('./lib/shapes');
 
 function writeFile (fileName, answers) {
     let svgString = ""; 
-    svgString = 
+    svgString = '<svg width="5cm" height="4cm" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    svgString += "<g>";
+    svgString += `${answers.shape}`;
+
+    
 }
