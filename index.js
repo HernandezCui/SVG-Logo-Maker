83668375b4905a96753ff551c1334a39f12ada55
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-26 00:19:43 -0800

    create Function that writes SVG

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
+
+
+// function that writes the svg file using user answers
+function writeToFile(filename, answers) {
+    let svgString = "";
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += '<g>';
+    svgString += '${answers.shape}';
+}
