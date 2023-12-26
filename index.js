3bfcfbbf62d0906145e9bcf4ebf0482bde476ad2
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-26 01:01:23 -0800

    created questions to create logo

--- index.js
+++ index.js
@@ -34,3 +34,35 @@ function writeToFile(fileName, answers) {
         err ? console.log(err) : console.log("Generated logo.svg");
     });
 }
+
+// function for user to answer questions in their command line 
+function promptUser() {
+    inquirer.prompt([
+        // text prompt
+        {  
+            type: "input", 
+            message: "What text would you like to display on your logo? (Maximum of three Characters)", 
+            name: "text", 
+        },
+        // text color 
+        {
+            type: "input",
+            message: "What color do you want your text to be? (Enter color keyword or hexadecimal number)", 
+            name: "textColor",
+        },
+        // Shape Choice
+        {
+            type: "list", 
+            message: "what shape would you like your logo to render?",
+            choices: ["Triangle", "Square", "Circle"],
+            name: "shape",
+        },
+        // Shape color
+        {
+            type: "input",
+            message: "What color would you like your shape to be? (Enter color keyword or hexadecimal number)",
+            name: "shapeBackgroundColor",
+        },
+    ])
+    
+}
