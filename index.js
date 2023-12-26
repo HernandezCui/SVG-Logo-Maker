3871d5193bbe188b65fd8c0ebc22e851f8b0dafb
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-25 22:54:30 -0800

    created class for Triangl shape

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
-
-function writeFile (fileName, answers) {
-    let svgString = ""; 
-    svgString = '<svg width="5cm" height="4cm" version="1.1" xmlns="http://www.w3.org/2000/svg">'
-    svgString += "<g>";
-    svgString += `${answers.shape}`;
-
-    
-}
