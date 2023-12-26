5d9f6187b494e6f05c4c785b9d86694e901c9067
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2023-12-26 04:27:16 -0800

    deleted old code & adding examples

--- index.js
+++ index.js
@@ -61,37 +61,3 @@ function promptUser() {
 
 // call promptUser 
 promptUser(); 
-
-
-
-// function that writes the svg file using user answers
-// function writeToFile(fileName, answers) {
-//     let svgString = "";
-//     svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-//     svgString += '<g>';
-//     svgString += '${answers.shape}';
-
-
-// // match users response to shape class to make new shapes
-//     let setShape;
-//     if (answers.shape === "Triangle") {
-//         setShape = new Triangle();
-//         svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}" />`;
-//     } else if (answers.shape === "Square") {
-//         setShape = new Square();
-//         svgString += `<rect width="200" height="200" fill="${answers.shapeBackgroundColor}" />`;
-//     } else {
-//         setShape = new Circle();
-//         svgString += `<circle cx="150" cy="100" r="100" fill="${answers.shapeBackgroundColor}" />`;
-//     }
-
-// // text tag for text alignment, color, and font size 
-//     svgString += `<text x="100" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-//     svgString += "</g>";
-//     svgString += "</svg>";
-
-// // genrating svg file using the file system 
-//     fs.writeFile(fileName, svgString, (err) => {
-//         err ? console.log(err) : console.log("Generated logo.svg");
-//     });
-// }
